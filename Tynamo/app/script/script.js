9c12a4a0ecb7967138a3e779f38f4d075e1341bb
19652-RussoGiuseppeCarmine <19652@studenti.marconiverona.edu.it>
2024-02-03 02:30:26 +0100

    Cambiamenti front-end

--- Tynamo/app/script/script.js
+++ Tynamo/app/script/script.js
@@ -1,10 +1,9 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
-const div = document.getElementById('registrazione');
-const div1 = document.getElementById('accesso');
-
-
+const registazione = document.getElementById('registrazione');
+const continua = document.getElementById('continua');
+const accesso = document.getElementById('accesso');
 
 
 registerBtn.addEventListener('click', () => {
@@ -15,11 +14,32 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-function prova() {
+function nascondi() {
 
-    if(div.style.display === 'none'){
-        div.style.display === 'block';
+    if(registazione.style.display === 'none'){
+        registazione.style.display === 'block';
     }else { 
-        div.style.display = 'none';
+        registazione.style.display = 'none';
+    }
+}
+
+
+function mostra() {
+
+    if(registazione.style.display === 'block'){
+        registazione.style.display === 'none';
+    }else { 
+        registazione.style.display = 'block';
+    }
+}
+
+
+function register() {
+
+    if(accesso.style.display === 'block'){
+        accesso.style.display === 'none' && continua.style.display === 'none';
+    }else { 
+        accesso.style.display = 'block'
+        continua.style.display = 'block';
     }
 }
