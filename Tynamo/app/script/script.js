943de439b8f43fe57401786ab6a993983c09893a
19652-RussoGiuseppeCarmine <19652@studenti.marconiverona.edu.it>
2024-02-03 01:43:16 +0100

    Miglioramento Front-end

--- Tynamo/app/script/script.js
+++ Tynamo/app/script/script.js
@@ -1,6 +1,11 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const div = document.getElementById('registrazione');
+const div1 = document.getElementById('accesso');
+
+
+
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,3 +14,12 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function prova() {
+
+    if(div.style.display === 'none'){
+        div.style.display === 'block';
+    }else { 
+        div.style.display = 'none';
+    }
+}
