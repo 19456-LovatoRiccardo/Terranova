01afb2f06dec4bb957c2e4b971f653b2763a25cd
19652-RussoGiuseppeCarmine <19652@studenti.marconiverona.edu.it>
2024-02-03 12:37:00 +0100

    Implementazione Front-end

--- Tynamo/app/script/script.js
+++ Tynamo/app/script/script.js
@@ -2,6 +2,8 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 const registazione = document.getElementById('registrazione');
+
+
 const continua = document.getElementById('continua');
 const accesso = document.getElementById('accesso');
 
@@ -14,12 +16,50 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-function nascondi() {
+
+function nascondiRegistrazione() {
 
     if(registazione.style.display === 'none'){
         registazione.style.display === 'block';
     }else { 
         registazione.style.display = 'none';
+    }
+}
+
+function nascondiAccesso() {
+
+    if(accesso.style.display === 'none'){
+        accesso.style.display === 'block';
+    }else { 
+        accesso.style.display = 'none';
+    }
+}
+
+function mostraAccesso() {
+
+    if(accesso.style.display === 'block'){
+        accesso.style.display === 'none';
+    }else { 
+        accesso.style.display = 'block';
+    }
+}
+
+
+function nascondiContinua() { //permette di nascondere il form continua prima che il tasto continua venga cliccato
+
+    if(continua.style.display === 'none'){
+        continua.style.display === 'block';
+    }else { 
+        continua.style.display = 'none';
+    }
+}
+
+function mostraContinua() {
+
+    if(continua.style.display === 'block'){
+        continua.style.display === 'none';
+    }else { 
+        continua.style.display = 'block';
     }
 }
 
@@ -34,12 +74,12 @@ function mostra() {
 }
 
 
-function register() {
+function registerRemoveAccesso() {
 
     if(accesso.style.display === 'block'){
-        accesso.style.display === 'none' && continua.style.display === 'none';
+        accesso.style.display === 'none';
     }else { 
-        accesso.style.display = 'block'
-        continua.style.display = 'block';
+        accesso.style.display = 'block';
     }
 }
+
