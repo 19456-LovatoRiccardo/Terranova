a2d704b8b2335f848a309b0477e219f8725a5883
Spaicrab <giuliodf2005@gmail.com>
2024-02-22 00:02:48 +0100

    Fixati typo

--- Tynamo/tynamo-java/src/main/java/com/grape/Tynamo/Main.java
+++ Tynamo/tynamo-java/src/main/java/com/grape/Tynamo/Main.java
@@ -1,6 +1,6 @@
 package com.grape.Tynamo;
 
-import org.pringframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
