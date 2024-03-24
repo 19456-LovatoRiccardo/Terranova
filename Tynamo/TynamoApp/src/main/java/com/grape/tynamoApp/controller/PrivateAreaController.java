9dda0cc37f731cd9b47fb9a476095dcfb59e7c48
Spaicrab <giuliodf2005@gmail.com>
2024-03-24 22:45:50 +0100

    Testando l'area privata

--- Tynamo/TynamoApp/src/main/java/com/grape/tynamoApp/controller/PrivateAreaController.java
+++ Tynamo/TynamoApp/src/main/java/com/grape/tynamoApp/controller/PrivateAreaController.java
@@ -1,10 +1,14 @@
 package com.grape.tynamoApp.controller;
 
+import com.grape.tynamoApp.config.JwtService;
 import com.grape.tynamoBackend.dao.DaoManager;
 import com.grape.tynamoBackend.domain.Anagrafica;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/privateArea")
 @RequiredArgsConstructor
 public class PrivateAreaController {
+    @Autowired
+    private final DaoManager DAO;
+    private final JwtService jwtService;
     
     @GetMapping("/anagrafica")
-    public Anagrafica anagrafica() {
-        return DaoManager.getEM().find(Anagrafica.class, 0);
+    public Anagrafica anagrafica(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        var account = DAO.getDaoAccount().getByEmail(jwtService.extractUsername(token));
+        return account.getAnagrafica();
     }
 }
