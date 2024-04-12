b64c7e8369c161d29cfa910eb45c4e471f3aa903
De Felice Giulio <20550@studenti.marconiverona.edu.it>
2024-04-12 13:51:07 +0200

    Implementato VITE + REACT al progetto per il front-end

---
+++ Tynamo/app/tynamo/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import LOGO from '../assets/LOGO.png'
+import './Navbar.css'
+import 'boxicons'
+
+function Navbar() {
+    return (
+      <>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+        <nav>
+            <input type="image" src={LOGO} class="logoHome" alt="LOGO"/>
+            <ul>
+                <li><a href="#">Chi siamo</a></li>
+                <li><a href="#">Offerte</a></li>
+                <li><a href="#">Contattaci</a></li>
+                <li class="login"><a href="login.html" class='bx bxs-user'></a></li>
+            </ul>
+        </nav>
+      </>
+    )
+}
+
+export default Navbar
